6b497114ce05819811f368341f2bb11d76a6fe0d
SanyaSho <sanyasho@worldspawn.party>
2025-04-13 12:00:23 +0300

    serializertest: Simplify way to add items

--- CMsgSOCacheSubscribedSerializerTest.py
+++ CMsgSOCacheSubscribedSerializerTest.py
@@ -5,9 +5,19 @@ from TFEnums import *
 
 class CMsgSOCacheSubscribedSerializerTest:
 	serializer = None
+	last_id = 1
+	last_slot = 1
 
 	def __init__( self, serializer ):
 		self.serializer = serializer
+
+	def add_item_to_inventory( self, item_data: dict ):
+		item_data.update( { "id": self.last_id, "slot": self.last_slot } )
+
+		self.serializer.add_item_to_inventory( item_data )
+
+		self.last_id += 1
+		self.last_slot += 1
 
 	def serialize_test_message( self ):
 		# Set default active loadout preset for each class
@@ -21,12 +31,8 @@ class CMsgSOCacheSubscribedSerializerTest:
 		self.serializer.add_class_loadout_preset( ETFClass.TF_CLASS_SPY,		ETFLoadoutPreset.TF_LOADOUT_PRESET_A )
 		self.serializer.add_class_loadout_preset( ETFClass.TF_CLASS_ENGINEER,		ETFLoadoutPreset.TF_LOADOUT_PRESET_A )
 
-		# Shitty values. Must start with 1
-		id = 1
-		slot = 1
 
-
-		def add_item_with_custom_texture( id: int = 1, slot: int = 1, itemid: int = -1, quality: int = EEconItemQuality.AE_NORMAL, ctlo: int = -1, cthi: int = -1 ):
+		def add_item_with_custom_texture( itemid: int = -1, quality: int = EEconItemQuality.AE_NORMAL, ctlo: int = -1, cthi: int = -1 ):
 			if itemid == -1 or ctlo == -1 or cthi == -1:
 				return
 
@@ -35,25 +41,20 @@ class CMsgSOCacheSubscribedSerializerTest:
 				self.serializer.attrib_helper.allocate_item_attribute_int_name( "custom texture hi", cthi )
 			]
 
-			self.serializer.add_item_to_inventory(
+			self.add_item_to_inventory(
 				{
-					"id": id, "slot": slot, "def_index": itemid,
+					"def_index": itemid,
 					"quality": quality,
 					"attributes": sign_attribs
 				}
 			)
 
-			# Must increment this everytime you touch the inventory
-			id += 1
-			slot += 1
-			return id, slot
-
 		# Add "Genuine Conscientious Objector" and "Genuine Photo Badge" to the inventory with custom texture
-		#(id, slot) = add_item_with_custom_texture( id, slot, 474, EEconItemQuality.AE_RARITY1, 378690501, 469905498 )
-		#(id, slot) = add_item_with_custom_texture( id, slot, 623, EEconItemQuality.AE_RARITY1, 378690501, 469905498 )
+		#add_item_with_custom_texture( 474, EEconItemQuality.AE_RARITY1, 378690501, 469905498 )
+		#add_item_with_custom_texture( 623, EEconItemQuality.AE_RARITY1, 378690501, 469905498 )
 
 
-		def add_item_with_paintkit( id: int = 1, slot: int = 1, itemid: int = -1 ):
+		def add_item_with_paintkit( itemid: int = -1 ):
 			paintkit_attribs = [
 				#self.serializer.attrib_helper.allocate_item_attribute_int_name( "paintkit_proto_def_index",	144 ),
 				#self.serializer.attrib_helper.allocate_item_attribute_int_name( "set_item_texture_wear",	1053609165 ),
@@ -67,65 +68,49 @@ class CMsgSOCacheSubscribedSerializerTest:
 				#self.serializer.attrib_helper.allocate_item_attribute_int_name( "custom_paintkit_seed_hi",	380547149 )	# 867
 			]
 
-			self.serializer.add_item_to_inventory(
+			self.add_item_to_inventory(
 				{
-					"id": id, "slot": slot, "def_index": itemid,
+					"def_index": itemid,
 					"attributes": paintkit_attribs
 				}
 			)
 
-			# Must increment this everytime you touch the inventory
-			id += 1
-			slot += 1
-			return id, slot
 
 		# Add Rocket Launcher with a paintkit applied
-		#(id, slot) = add_item_with_paintkit( id, slot, 205 )
+		#add_item_with_paintkit( 205 )
 
 
-		def give_some_painted_weapons( id: int = 1, slot: int = 1, itemid: int = 1, max: int = 1 ):
+		def give_some_painted_weapons( itemid: int = 1, max: int = 1 ):
 			for paint in range( max ):
 				attributes = [
 					self.serializer.attrib_helper.allocate_item_attribute_int_name( "paintkit_proto_def_index",	paint )
 				]
 
-				self.serializer.add_item_to_inventory(
+				self.add_item_to_inventory(
 					{
-						"id": id, "slot": slot, "def_index": itemid,
+						"def_index": itemid,
 						"attributes": attributes
 					}
 				)
 
-				# Must increment this everytime you touch the inventory
-				id += 1
-				slot += 1
-
-			return id, slot
-
-		#(id, slot) = give_some_painted_weapons( id, slot, 205, 512 )
+		#give_some_painted_weapons( 205, 512 )
 
 
-		def give_all_taunts( id: int = 1, slot: int = 1 ):
+		def give_all_taunts():
 			for i in self.serializer.isp.get_all_items():
 				item = self.serializer.isp.get_all_items()[i]
 
 				if ("prefab" in item and "taunt" in item["prefab"]) or ("taunt" in item) or ("item_slot" in item and "taunt" in item["item_slot"]):
 					self.serializer.add_item_to_inventory(
 						{
-							"id": id, "slot": slot, "def_index": i
+							"def_index": i
 						}
 					)
 
-					# Must increment this everytime you touch the inventory
-					id += 1
-					slot += 1
-
-			return id, slot
-
-		#(id, slot) = give_all_taunts( id, slot )
+		#give_all_taunts()
 
 
-		def give_valve_rocket_launcher( id: int = 1, slot: int = 1 ):
+		def give_valve_rocket_launcher():
 			# taken from TF2C b4 items_game.txt
 			valve_rocket_launcher_attributes = [
 				# Unusual Effect: Flying Bits
@@ -157,68 +142,51 @@ class CMsgSOCacheSubscribedSerializerTest:
 				#self.serializer.add_item_to_class( ETFClass.TF_CLASS_SOLDIER, loadout_positions_t.LOADOUT_POSITION_PRIMARY )
 			]
 
-			self.serializer.add_item_to_inventory(
+			self.add_item_to_inventory(
 				{
-					"id": id, "slot": slot, "def_index": 18,
+					"def_index": 18,
 					"level": EEconConstants.MAX_ITEM_LEVEL, "quality": EEconItemQuality.AE_DEVELOPER,
 					"attributes": valve_rocket_launcher_attributes, "equipped_on": valve_rocket_launcher_equipped_on
 				}
 			)
 
-			# Must increment this everytime you touch the inventory
-			id += 1
-			slot += 1
-			return id, slot
-
-		(id, slot) = give_valve_rocket_launcher( id, slot )
+		give_valve_rocket_launcher()
 
 
-		def give_item_with_all_qualities( item: int, id: int = 1, slot: int = 1 ):
+		def give_item_with_all_qualities( item: int ):
 			for quality in range( EEconItemQuality.AE_MAX_TYPES ):
-				self.serializer.add_item_to_inventory(
+				self.add_item_to_inventory(
 					{
-						"id": id, "slot": slot, "def_index": item,
+						"def_index": item,
 						"level": EEconConstants.MAX_ITEM_LEVEL, "quality": quality
 					}
 				)
 
-				# Must increment this everytime you touch the inventory
-				id += 1
-				slot += 1
-
-			return id, slot
-
-		#(id, slot) = give_item_with_all_qualities( 18, id, slot )
-		#(id, slot) = give_item_with_all_qualities( 1071, id, slot )
+		#give_item_with_all_qualities( 18 )
+		#give_item_with_all_qualities( 1071 )
 
 
-		def give_item_with_unusual_effect( item: int, effect_list: dict, id: int = 1, slot: int = 1 ):
+		def give_item_with_unusual_effect( item: int, effect_list: dict ):
 			for i in effect_list:
 				effect_attributes = [
 					self.serializer.attrib_helper.allocate_item_attribute_float_name( "attach particle effect",	float( i ) ),
 					self.serializer.attrib_helper.allocate_item_attribute_string_name( "custom name attr",		f"Effect: {i}" )
 				]
 
-				self.serializer.add_item_to_inventory(
+				self.add_item_to_inventory(
 					{
-						"id": id, "slot": slot, "def_index": item,
+						"def_index": item,
 						"level": EEconConstants.MAX_ITEM_LEVEL, "quality": EEconItemQuality.AE_UNUSUAL,
 						"attributes": effect_attributes
 					}
 				)
 
-				# Must increment this everytime you touch the inventory
-				id += 1
-				slot += 1
-
-			return id, slot
-
-		#(id, slot) = give_item_with_unusual_effect( 125, self.serializer.isp.get_cosmetic_unusual_effects(), id, slot )
-		##(id, slot) = give_item_with_unusual_effect( 125, self.serializer.isp.get_killstreak_eyeglows(), id, slot )
-		#(id, slot) = give_item_with_unusual_effect( 13, self.serializer.isp.get_weapon_unusual_effects(), id, slot )
+		#give_item_with_unusual_effect( 125, self.serializer.isp.get_cosmetic_unusual_effects() )
+		##give_item_with_unusual_effect( 125, self.serializer.isp.get_killstreak_eyeglows() )
+		#give_item_with_unusual_effect( 13, self.serializer.isp.get_weapon_unusual_effects() )
 
 
-		def give_australium_item( item: int, id: int = 1, slot: int = 1 ):
+		def give_australium_item( item: int ):
 			# https://forums.alliedmods.net/showthread.php?t=141962&page=121
 			australium_attributes = [
 				#"2027 ; 1 ; 2022 ; 1 ; 542 ; 1"
@@ -226,24 +194,19 @@ class CMsgSOCacheSubscribedSerializerTest:
 				self.serializer.attrib_helper.allocate_item_attribute_int_name( "loot rarity",		1 ),
 				self.serializer.attrib_helper.allocate_item_attribute_int_name( "item style override",	1 )
 			]
-			self.serializer.add_item_to_inventory(
+			self.add_item_to_inventory(
 				{
-					"id": id, "slot": slot, "def_index": item,
+					"def_index": item,
 					"quality": EEconItemQuality.AE_STRANGE,
 					"attributes": australium_attributes
 				}
 			)
 
-			# Must increment this everytime you touch the inventory
-			id += 1
-			slot += 1
-			return id, slot
-
-		#(id, slot) = give_australium_item( 200, id, slot )
-		#(id, slot) = give_australium_item( 205, id, slot )
+		#give_australium_item( 200 )
+		#give_australium_item( 205 )
 
 
-		def give_all_items( id: int = 1, slot: int = 1 ):
+		def give_all_items():
 			def get_random_item_level():
 				return random.randrange( EEconConstants.MIN_ITEM_LEVEL, EEconConstants.MAX_ITEM_LEVEL, 1 )
 
@@ -259,53 +222,35 @@ class CMsgSOCacheSubscribedSerializerTest:
 				if "prefab" in item and "tournament_medal" in item["prefab"]:
 					continue
 
-				#slot_banlist = [ "hat" ]
-				#if "item_slot" in item and item["item_slot"] in slot_banlist:
-				#	continue
-
-				#prefab_banlist = [ "base_hat", "hat", "tournament_medal" ]
-				#if "prefab" in item:
-				#	for j in item["prefab"].split( " " ):
-				#		if j in prefab_banlist:
-				#			continue
-
 				quality = get_quality_from_schema_string( item["item_quality"] ) if "item_quality" in item else EEconItemQuality.AE_NORMAL
 
 				attributes = [
 					#self.serializer.attrib_helper.allocate_item_attribute_string_name( "custom desc attr", f"EconID: {i}" )
 				]
 
-				self.serializer.add_item_to_inventory(
+				self.add_item_to_inventory(
 					{
-						"id": id, "slot": slot, "def_index": int( i ),
+						"def_index": int( i ),
 						"level": get_random_item_level(), "quality": quality,
 						"attributes": attributes
 					}
 				)
 
-				# Must increment this everytime you touch the inventory
-				id += 1
-				slot += 1
-
-			return id, slot
-
-		#(id, slot) = give_all_items( id, slot )
+		#give_all_items()
 
 
 		# Add the "World Traveler's Hat"
-		self.serializer.add_item_to_inventory( { "id": id, "slot": slot, "def_index": 1899, "level": 20, "quality": EEconItemQuality.AE_UNIQUE } )
-		# Must increment this everytime you touch the inventory
-		id += 1
-		slot += 1
+		self.add_item_to_inventory( { "def_index": 1899, "level": 20, "quality": EEconItemQuality.AE_UNIQUE } )
 
 
-		print( f"Total items added: {id - 1}, used slots: {slot - 1}" )
+		print( f"Total items added: {self.last_id - 1}, used slots: {self.last_slot - 1}" )
 
 
 		# Append player info
 		self.serializer.add_client_info(
 			{ "additional_backpack_slots": EEconConstants.MAX_NUM_FULL_BACKPACK_SLOTS }
 		)
+
 
 		# Add max contribution level for each map token (used by World Traveler's Hat)
 		for def_index in self.serializer.isp.get_all_items():
